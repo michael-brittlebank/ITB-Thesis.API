7fb5651106c254f0a1328dded1be179bc958665e
mike-stumpf <mike.a.stumpf@gmail.com>
2017-02-02 16:53:35 +0000

    core - added logging to mysql connection

--- services/mysql.js
+++ services/mysql.js
@@ -1,11 +1,15 @@
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host : process.env.MYSQL_HOST,
-        user : process.env.MYSQL_USER,
-        password : process.env.MYSQL_PASSWORD,
-        database : process.env.MYSQL_DATABASE
-    }
-});
+const //services
+    utilService = require('./util'),
+    //packages
+    knex = require('knex')({
+        client: 'mysql',
+        connection: {
+            host : process.env.MYSQL_HOST,
+            user : process.env.MYSQL_USER,
+            password : process.env.MYSQL_PASSWORD,
+            database : process.env.MYSQL_DATABASE
+        },
+        debug: utilService.isLocalConfig()
+    });
 
 module.exports = knex;
